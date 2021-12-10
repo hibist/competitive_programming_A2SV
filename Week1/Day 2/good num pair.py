3fd46b837ff0a6451180708a50419c734695ac7d
hibist <hibistmar@gmail.com>
2021-12-10 23:06:52 +0400

    exersice2

---
+++ Week1/Day 2/good num pair.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def numIdenticalPairs(self,nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+    n=[]
+    for i in range(0,len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i]==nums[j]:
+                n.append()
