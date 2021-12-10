b3958869b8ef85f27b0ffdb090c161e8373c06f2
hibist <hibistmar@gmail.com>
2021-12-10 23:47:37 +0400

    numPairExercise

--- Week1/DAY2/Num_Pair.py
+++ Week1/DAY2/Num_Pair.py
@@ -1,9 +1,6 @@
 class Solution(object):
     def numIdenticalPairs(self,nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+      
         n=0
         for i in range(0,len(nums)):
             for j in range(i+1,len(nums)):
