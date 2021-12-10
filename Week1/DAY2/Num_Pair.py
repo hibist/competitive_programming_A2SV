0152adc23c92212730df5d35388e04b6649cbdf5
hibist <hibistmar@gmail.com>
2021-12-10 23:41:04 +0400

    Num_pair_exercise

---
+++ Week1/DAY2/Num_Pair.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def numIdenticalPairs(self,nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=0
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]==nums[j] and i<j:
+                    n+=1
+        return n
